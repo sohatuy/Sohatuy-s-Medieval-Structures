3e6969d1a0b2ada0449e967af066b175e6b45de5
Sohatuy <sohatuy993@gmail.com>
2025-10-17 21:33:25 +0300

    Adding hermits table

--- src/main/java/com/sohatuy/medieval_structures/ModBlocks/HermitsTable.java
+++ src/main/java/com/sohatuy/medieval_structures/ModBlocks/HermitsTable.java
@@ -5,31 +5,65 @@ import javax.annotation.Nullable;
 
 import com.sohatuy.medieval_structures.init.BlockInit;
 
-import net.minecraft.world.entity.player.Player; // Добавь этот импорт
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.Level; // Добавь этот импорт
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.Level;
 
-public class HermitsTable extends Block{
+public class HermitsTable extends Block {
+    // Добавляем свойство направления
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+
     public HermitsTable() {
-        super(BlockBehaviour.Properties.of()
-        .destroyTime(1)
-        .sound(SoundType.WOOD)
-        );
+        super(Properties.of()
+                .mapColor(MapColor.WOOD)
+                .destroyTime(1)
+                .sound(SoundType.WOOD));
+        
+        // Устанавливаем начальное состояние по умолчанию
+        this.registerDefaultState(this.stateDefinition.any()
+                .setValue(FACING, Direction.NORTH));
     }
-    
+
+    // Создаем состояние блока при установке
     @Override
-public void playerDestroy(@Nonnull Level level, @Nonnull Player player, @Nonnull BlockPos pos, @Nonnull BlockState state, @Nullable BlockEntity blockEntity, @Nonnull ItemStack tool) {
-    super.playerDestroy(level, player, pos, state, blockEntity, tool);
-    
-    if (!level.isClientSide) {
-        // Самый простой и правильный способ
-        popResource(level, pos, new ItemStack(BlockInit.HERMITS_TABLE.get()));
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState()
+                .setValue(FACING, context.getHorizontalDirection().getOpposite());
     }
-}
+
+    // Регистрируем свойства блока
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    // Добавляем вращение для визуального отображения
+    @Override
+    public BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirror) {
+        return state.rotate(mirror.getRotation(state.getValue(FACING)));
+    }
+
+    @Override
+    public void playerDestroy(@Nonnull Level level, @Nonnull Player player, @Nonnull BlockPos pos, @Nonnull BlockState state, @Nullable BlockEntity blockEntity, @Nonnull ItemStack tool) {
+        super.playerDestroy(level, player, pos, state, blockEntity, tool);
+        
+        if (!level.isClientSide) {
+            popResource(level, pos, new ItemStack(BlockInit.HERMITS_TABLE.get()));
+        }
+    }
 }
