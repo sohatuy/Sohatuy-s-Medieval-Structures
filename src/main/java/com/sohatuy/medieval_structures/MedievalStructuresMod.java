0d61eb4164f6f5440d304fffc747574214f8e298
Sohatuy <sohatuy993@gmail.com>
2025-10-18 20:49:45 +0300

    Фввштп ашдуы

--- src/main/java/com/sohatuy/medieval_structures/MedievalStructuresMod.java
+++ src/main/java/com/sohatuy/medieval_structures/MedievalStructuresMod.java
@@ -2,7 +2,7 @@ package com.sohatuy.medieval_structures;
 
 import com.sohatuy.medieval_structures.init.BlockInit;
 import com.sohatuy.medieval_structures.init.ItemInit;
-import com.sohatuy.medieval_structures.init.POITypesInit;
+import com.sohatuy.medieval_structures.init.POITypesInit; // ДОБАВЬТЕ ЭТОТ ИМПОРТ
 import com.sohatuy.medieval_structures.init.VillagerInit;
 
 import net.minecraftforge.api.distmarker.Dist;
@@ -15,50 +15,37 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(MedievalStructuresMod.MODID)
-public class MedievalStructuresMod
-{
-    // Define mod id in a common place for everything to reference
+public class MedievalStructuresMod {
     public static final String MODID = "medieval_structures";
 
-    // Creates a creative tab with the id "examplemod:example_tab" for the example item, that is placed after the combat tab
-
-    public MedievalStructuresMod(FMLJavaModLoadingContext context)
-    {
+    public MedievalStructuresMod(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
 
-        // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
+        // Регистрируем ВСЕ deferred registers в правильном порядке
         BlockInit.BLOCKS.register(modEventBus);
         ItemInit.ITEMS.register(modEventBus);
+        POITypesInit.POI_TYPES.register(modEventBus); // ← ДОБАВЬТЕ ЭТУ СТРОЧКУ!
         VillagerInit.VILLAGER_PROFESSIONS.register(modEventBus);
-        POITypesInit.POI_TYPES.register(modEventBus);
 
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
+    private void commonSetup(final FMLCommonSetupEvent event) {
         // Some common setup code
-        
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
+    public void onServerStarting(ServerStartingEvent event) {
         
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
+    public static class ClientModEvents {
         @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
+        public static void onClientSetup(FMLClientSetupEvent event) {
             
         }
     }
